ea893ca64de46d71fa72a76c875bc0fd212e967a
Félix Defrance <felixgif@gmail.com>
2017-04-18 00:14:24 +0200

    Update enigma-short.py

--- enigma-short.py
+++ enigma-short.py
@@ -1,12 +1,9 @@
-#enigma
-
 alphabetList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ', "*"]
 alphabetDict = {'G': 7, 'U': 21, 'T': 20, 'L': 12, 'Y': 25, 'Q': 17, 'V': 22, 'J': 10, 'O': 15, 'W': 23, 'N': 14, 'R': 18, 'Z': 26, 'S': 19, 'X': 24, 'A': 1, 'M': 13, 'E': 5, 'D': 4, 'I': 9, 'F': 6, 'P': 16, 'B': 2, 'H': 8, 'K': 11, 'C': 3}
 rotor1 = ['J','G','D','Q','O','X','U','S','C','A','M','I','F','R','V','T','P','N','E','W','K','B','L','Z','Y','H']
 rotor2 = ['N','T','Z','P','S','F','B','O','K','M','W','R','C','J','D','I','V','L','A','E','Y','U','X','H','G','Q']
 rotor3 = ['J','V','I','U','B','H','T','C','D','Y','A','K','E','Q','Z','P','O','S','G','X','N','R','M','W','F','L']
 reflector = ['Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T']
-
 def rotationRotor(liste1):
 	liste1.append(liste1[0])
 	del liste1[0]
@@ -17,7 +14,6 @@ def estValide(liste1):
 		if alphabetList.count(elt.upper()) < 1:
 			return False
 	return True
-
 print("Welcome to the enigma machine")
 messageList = []
 while estValide(messageList) == False or len(messageList) <= 0:
