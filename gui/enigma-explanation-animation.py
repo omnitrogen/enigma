a97a7ec81f566e068f8ce79830f9bad149b22ac5
Félix Defrance <felixgif@gmail.com>
2017-05-12 23:23:45 +0200

    decalage de ligneDepart ds l'animation

--- gui/enigma-explanation-animation.py
+++ gui/enigma-explanation-animation.py
@@ -60,23 +60,24 @@ rectangle2 = canvas.create_rectangle(0, 408, 1440, 900, fill="white", width=0)
 canvas.pack()
 
 i1,i2,i3 = 0,0,0
-oval1,oval2,oval3,oval4,oval5,oval6 = canvas.create_line(0,0,0,0),canvas.create_line(0,0,0,0),canvas.create_line(0,0,0,0),canvas.create_line(0,0,0,0),canvas.create_line(0,0,0,0),canvas.create_line(0,0,0,0)
-
+animateIncremente = 35
 def animate(line, sens):
+    global animateIncremente
     coord = canvas.coords(line)
     coord = [int(x) for x in coord]
     if sens:
-        lineAnimate = canvas.create_line(coord[0],coord[1],coord[0],coord[1],fill="red", width="5",tags="666")
+        canvas.create_line(coord[0],coord[1],coord[0],coord[1],fill="red", width="5")
         for elt in range(coord[0], coord[2]+1,1):
-            canvas.coords(lineAnimate, coord[0],coord[1], elt, coord[1])
+            canvas.coords(str(animateIncremente), coord[0],coord[1], elt, coord[1])
             time.sleep(0.0000001)
             canvas.update()
     if not sens:
-        lineAnimate = canvas.create_line(coord[2],coord[1],coord[2],coord[1],fill="green", width="5",tags="666")
+        canvas.create_line(coord[2],coord[1],coord[2],coord[1],fill="green", width="5")
         for elt in range(coord[2]-1, coord[0],-1):
-            canvas.coords(lineAnimate, coord[2],coord[1], elt, coord[1])
+            canvas.coords(str(animateIncremente), coord[2],coord[1], elt, coord[1])
             time.sleep(0.0000001)
             canvas.update()
+    animateIncremente += 1
 
 def rotate(liaisonsCanvas):
     global i1,i2,i3
@@ -119,6 +120,10 @@ def circle(numLigne,color):
         canvas.create_oval(54,107,81,134, outline=color, width="2")
     canvas.update()
 
+def effacerLignes():
+    canvas.delete('35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '119', '120', '121', '122', '123', '124', '125', '126', '127', '128', '129', '130', '131', '132', '133', '134', '135', '136', '137', '138', '139', '140', '141', '142', '143', '144', '145', '146', '147', '148', '149', '150', '151', '152', '153', '154', '155', '156', '157', '158', '159', '160', '161', '162', '163', '164', '165', '166', '167', '168', '169', '170', '171', '172', '173', '174', '175', '176', '177', '178', '179', '180', '181', '182', '183', '184', '185', '186', '187', '188', '189', '190', '191', '192', '193', '194', '195', '196', '197', '198', '199')
+    canvas.update()
+
 def codePath(event=None):
     lettresEntrees = list(entryvar.get())
     nbRotation = 0
@@ -143,7 +148,7 @@ def codePath(event=None):
     r3D = [3,6,1,5,2,4]
 
     for elt in lettresEntrees:
-        ligneDepart = alphabet.index(elt) + 1
+        ligneDepart = alphabet.index(str.upper(elt)) + 1
         a = r1G[ligneDepart-1] - i1
         b = r2G[a-1] - i2
         c = r3G[b-1] - i3
@@ -175,9 +180,8 @@ def codePath(event=None):
             rotate(liaisonsCanvas3)
             rotationRotor(r3G)
             rotationRotor(r3D)
-        time.sleep(5)
-        canvas.delete("666")
-        canvas.update()
+        effacerLignes()
+        time.sleep(6)
         nbRotation += 1
 
 
@@ -206,7 +210,6 @@ coderPath.pack(padx=5, pady=5)
 
 buttonReinit = Button(root,text="Reinitialiser", command=reinitialiser)
 buttonReinit.pack(padx=5, pady=5)
-
 
 #coded with <3 by omnitrogen
 
