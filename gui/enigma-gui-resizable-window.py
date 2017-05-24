80adfd3d4198da30c0c18e5770b16f53900d6a03
Félix Defrance <felixgif@gmail.com>
2017-05-24 21:23:53 +0200

    update

--- gui/enigma-gui-resizable-window.py
+++ gui/enigma-gui-resizable-window.py
@@ -1,4 +1,4 @@
-#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+#!/usr/bin/python3
 from tkinter import *
 from tkinter.messagebox import *
 fenetre = Tk()
@@ -14,13 +14,15 @@ class AutoScrollbar(Scrollbar):
         else:
             self.grid()
         Scrollbar.set(self, lo, hi)
+
     def pack(self, **kw):
         raise(TclError, "can't pack this widget")
+
     def place(self, **kw):
         raise(TclError, "can't grid this widget")
 
 vscrollbar = AutoScrollbar(fenetre)
-vscrollbar.grid(row=0,  column=1, sticky=N+S)
+vscrollbar.grid(row=0, column=1, sticky=N+S)
 hscrollbar = AutoScrollbar(fenetre, orient=HORIZONTAL)
 hscrollbar.grid(row=1, column=0, sticky=E+W)
 
@@ -47,7 +49,7 @@ Label(frame, image=image, background="white", borderwidth=0).pack(padx=10, pady=
 
 #help_button
 def help():
-    showinfo( "The Enigma Machine Quick Start", "Hello World!\nThis is a quick tutorial on how to use this app!\nFirst, you need to choose the order of the rotors.\nThen you need to set the rotors' position\nYou can finally write your message and encrypt it by pressing the Return key!\nThat's it, you've just encrypt your first enigma message!\n                              Have fun!")
+    showinfo("The Enigma Machine Quick Start", "Hello World!\nThis is a quick tutorial on how to use this app!\nFirst, you need to choose the order of the rotors.\nThen you need to set the rotors' position\nYou can finally write your message and encrypt it by pressing the Return key!\nThat's it, you've just encrypt your first enigma message!\n                              Have fun!")
 helpButton = Button(frame, text ="Help! Quick Start", command = help, background="white")
 helpButton.pack(padx=5, pady=5)
 
@@ -94,7 +96,7 @@ frameRotor.pack()
 #frame_to_set_rotor_position
 frame1 = Frame(frame, borderwidth=0, relief=FLAT, background='white')
 frame1.pack(side=TOP, padx=10, pady=10)
-    
+
 def update1(x):
     x = int(x)
     alphabetList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -177,28 +179,33 @@ def code(event=None):
     elif var6list[5] == '3':
         rotor3 = ['J','V','I','U','B','H','T','C','D','Y','A','K','E','Q','Z','P','O','S','G','X','N','R','M','W','F','L']
 
-    alphabetList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ', "'"]
+    alphabetList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
     alphabetDict = {'G': 7, 'U': 21, 'T': 20, 'L': 12, 'Y': 25, 'Q': 17, 'V': 22, 'J': 10, 'O': 15, 'W': 23, 'N': 14, 'R': 18, 'Z': 26, 'S': 19, 'X': 24, 'A': 1, 'M': 13, 'E': 5, 'D': 4, 'I': 9, 'F': 6, 'P': 16, 'B': 2, 'H': 8, 'K': 11, 'C': 3}
     #rotor1 = ['J','G','D','Q','O','X','U','S','C','A','M','I','F','R','V','T','P','N','E','W','K','B','L','Z','Y','H']
     #rotor2 = ['N','T','Z','P','S','F','B','O','K','M','W','R','C','J','D','I','V','L','A','E','Y','U','X','H','G','Q']
     #rotor3 = ['J','V','I','U','B','H','T','C','D','Y','A','K','E','Q','Z','P','O','S','G','X','N','R','M','W','F','L']
     reflector = ['Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T']
-    
+
     for loop1 in range(a):
         rotationRotor(rotor1)
     for loop2 in range(b):
         rotationRotor(rotor2)
     for loop3 in range(c):
         rotationRotor(rotor3)
+
     sortieListe = list(sortie)
+    print(sortieListe)
+    sortieListe = [x for x in sortieListe if x != " "]
+    print(sortieListe)
+
     if not estValide(sortieListe):
-        value = StringVar() 
+        value = StringVar()
         value.set('Please enter only letters and spaces!')
         liste.insert(END, value.get())
         liste.itemconfig(END, {'bg':'red'})
         liste.see("end")
     elif (var4list[5] == var5list[5] == var6list[5]) or (var4list[5] == var5list[5]) or (var4list[5] == var6list[5]) or (var5list[5] == var6list[5]):
-        value = StringVar() 
+        value = StringVar()
         value.set('You can only use a rotor once!')
         liste.insert(END, value.get())
         liste.itemconfig(END, {'bg':'red'})
@@ -206,37 +213,32 @@ def code(event=None):
     else:
         s = []
         for i in range(0,len(sortieListe),1):
-            if sortieListe[i] == ' ':
-                s.append(' ')
-            elif sortieListe[i] == "'":
-                s.append("'")
-            else:
-                a = alphabetDict[sortieListe[i].upper()]
-                b = rotor1[a-1]
-                c = alphabetDict[b]
-                d = rotor2[c-1]
-                e = alphabetDict[d]
-                f = rotor3[e-1]
-                g = alphabetDict[f]
-                h = reflector[g-1]
-                j = rotor3.index(h)
-                k = alphabetList[j]
-                l = rotor2.index(k)
-                m = alphabetList[l]
-                n = rotor1.index(m)
-                o = alphabetList[n]
-                s.append(o)
-            if (i+1)%1 == 0:
+            a = alphabetDict[sortieListe[i].upper()]
+            b = rotor1[a-1]
+            c = alphabetDict[b]
+            d = rotor2[c-1]
+            e = alphabetDict[d]
+            f = rotor3[e-1]
+            g = alphabetDict[f]
+            h = reflector[g-1]
+            j = rotor3.index(h)
+            k = alphabetList[j]
+            l = rotor2.index(k)
+            m = alphabetList[l]
+            n = rotor1.index(m)
+            o = alphabetList[n]
+            s.append(o)
+            if (i+1) % 1 == 0:
                 rotationRotor(rotor1)
-            if (i+1)%26 == 0:
+            if (i+1) % 26 == 0:
                 rotationRotor(rotor2)
-            if (i+1)%676 == 0:
+            if (i+1) % 676 == 0:
                 rotationRotor(rotor3)
-        value = StringVar() 
+        value = StringVar()
         value.set(''.join(s))
         liste.insert(END, value.get())
         liste.see("end")
-    
+
 #text_entry
 entryvar = StringVar()
 entry = Entry(frame, textvariable = entryvar, width=50, background="white")
@@ -253,14 +255,14 @@ b1 = Button(frame, text="(de)code", width=10, command=code, background="white")
 b1.pack()
 
 #store_results
-f1 = Frame(frame) 
-s1 = Scrollbar(f1) 
+f1 = Frame(frame)
+s1 = Scrollbar(f1)
 liste = Listbox(f1, height=5, width=50, borderwidth=0, background='white')
-s1.config(command = liste.yview) 
-liste.config(yscrollcommand = s1.set) 
-liste.pack(side = LEFT, fill = Y, padx=5, pady=5) 
-s1.pack(side = RIGHT, fill = Y) 
-f1.pack() 
+s1.config(command = liste.yview)
+liste.config(yscrollcommand = s1.set)
+liste.pack(side = LEFT, fill = Y, padx=5, pady=5)
+s1.pack(side = RIGHT, fill = Y)
+f1.pack()
 
 #button_to_clear_listbox
 b2 = Button(frame, text="clear list", width=10, command=clear, background='white')
@@ -281,4 +283,3 @@ frame.update_idletasks()
 canvas.config(scrollregion=canvas.bbox("all"))
 
 mainloop()
-
